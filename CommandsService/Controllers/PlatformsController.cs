9eda2f0b7dee06a0a4f5f50b1fd5425011dbfb2c
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2023-06-02 11:20:31 -0400

    Modifiying the launch settings and testing

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+  [Route("api/c/[controller]")]
+  [ApiController]
+  public class PlatformsController : ControllerBase
+  {
+    public PlatformsController()
+    {
+
+    }
+    public ActionResult TestInboundConnection()
+    {
+      Console.WriteLine("Inbound POST #Command Service");
+      return Ok("Inbound Test from PlatformsController");
+    }
+  }
+}
